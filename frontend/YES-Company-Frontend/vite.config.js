95558b88909818d25b418750d2dceadd67f4564c
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-20 16:21:48 +0700

    initialize the React JS project for Frontend, and change the default port 5173 of Vite to port 3000 in vite.config.js

---
+++ frontend/YES-Company-Frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  //change port for production
+  preview: {
+    port: 3000,
+  },
+  // for dev
+  server: {
+    port: 3000,
+  },
+});
