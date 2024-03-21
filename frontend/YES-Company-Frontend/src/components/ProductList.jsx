8cb695ea01661580fa0077f4e8c2425e9f368171
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-21 19:34:46 +0700

    update the ProductList component to receive the products list from its LandingPage parent component, and just render that products list

--- frontend/YES-Company-Frontend/src/components/ProductList.jsx
+++ frontend/YES-Company-Frontend/src/components/ProductList.jsx
@@ -1,49 +1,48 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useState, useEffect } from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import { useState, useEffect } from "react";
 import CardProduct from "./CardProduct";
-import SpinnerLoading from "./utils/SpinnerLoading";
+// import SpinnerLoading from "./utils/SpinnerLoading";
 
-const ProductList = () => {
-  const [products, setProducts] = useState([]);
-  const { isAuthenticated } = useAuth0();
-  const [token, setToken] = useState(localStorage.getItem("token"));
-  const [isLoading, setIsLoading] = useState(true);
+const ProductList = ({ products }) => {
+  //   const { isAuthenticated } = useAuth0();
+  //   const [token, setToken] = useState(localStorage.getItem("token"));
+  //   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (isAuthenticated && token) {
-      getProducts();
-    }
-  }, [isAuthenticated, token]);
-  console.log("outside return: ", products);
-  console.log("outside return: ", products.length);
-  const getProducts = async () => {
-    try {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/products", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const responseData = await response.json();
-      setIsLoading(false);
-      setProducts(responseData.products);
-      console.log(responseData);
-      console.log(responseData.products);
-      console.log("inside getProduct: ", products.length);
-    } catch (error) {
-      console.log("error");
-      console.log(error.error);
-      setIsLoading(false);
-    }
-  };
+  //   useEffect(() => {
+  //     if (isAuthenticated && token) {
+  //       getProducts();
+  //     }
+  //   }, [isAuthenticated, token]);
+  //   console.log("outside return: ", products);
+  //   console.log("outside return: ", products.length);
+  //   const getProducts = async () => {
+  //     try {
+  //       setIsLoading(true);
+  //       const response = await fetch("http://localhost:8080/products", {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       const responseData = await response.json();
+  //       setIsLoading(false);
+  //       setProducts(responseData.products);
+  //       console.log(responseData);
+  //       console.log(responseData.products);
+  //       console.log("inside getProduct: ", products.length);
+  //     } catch (error) {
+  //       console.log("error");
+  //       console.log(error.error);
+  //       setIsLoading(false);
+  //     }
+  //   };
 
   return (
     <div>
-      {isLoading && <SpinnerLoading></SpinnerLoading>}
+      {/* {isLoading && <SpinnerLoading></SpinnerLoading>} */}
       <h1>Product List</h1>
       {console.log(products)}
       {console.log(products.length)}
