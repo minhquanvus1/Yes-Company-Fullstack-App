efb42ed415cd63aba0017bed15c8f41ff5f5f8cc
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-21 19:33:48 +0700

    update the SearchProduct component to have the correct API URL, and pass the correct props to it, and define the handleSubmit() function for submit event

--- frontend/YES-Company-Frontend/src/components/SearchProduct.jsx
+++ frontend/YES-Company-Frontend/src/components/SearchProduct.jsx
@@ -1,43 +1,50 @@
 import React from "react";
 import { useState } from "react";
 
-const SearchProduct = () => {
+const SearchProduct = ({ token, setProducts }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
+    console.log(searchTerm);
   };
 
   const searchForProduct = async () => {
     try {
-      const response = await fetch(
-        `http://localhost:8080/products?search=${searchTerm}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            name: searchTerm,
-          }),
-        }
-      );
-      if (!response.ok) {
+      const response = await fetch(`http://localhost:8080/search-products`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name: searchTerm,
+        }),
+      });
+      if (response.status === 404) {
+        console.log("Product not found");
+        setProducts([]);
+      } else if (response.status === 200) {
+        const responseData = await response.json();
+        console.log(responseData);
+        const productList = responseData.products;
+        setProducts(productList);
+        console.log(productList);
+      } else {
         throw new Error("Network response was not ok");
       }
-      const responseData = await response.json();
-      console.log(responseData);
-      const productList = responseData.products;
-      console.log(productList);
     } catch (error) {
       console.log("error");
       console.log(error.message);
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    searchForProduct();
+  };
   return (
     <>
-      <form className="d-flex">
+      <form className="d-flex" onSubmit={handleSubmit}>
         <input
           className="form-control me-2"
           type="search"
@@ -46,11 +53,7 @@ const SearchProduct = () => {
           value={searchTerm}
           onChange={handleChange}
         />
-        <button
-          className="btn btn-outline-success"
-          type="submit"
-          onClick={searchForProduct}
-        >
+        <button className="btn btn-outline-success" type="submit">
           Search
         </button>
       </form>
