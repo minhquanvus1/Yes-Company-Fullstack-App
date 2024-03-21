5dfccd39f12e2f9e7e6232ec933fe6b91233b8b7
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-21 10:00:57 +0700

    create CreateProductForm component for Manager to create new Product

---
+++ frontend/YES-Company-Frontend/src/components/CreateProductForm.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { useState } from "react";
+
+const CreateProductForm = () => {
+  const [name, setName] = useState("");
+  const [unit_price, setUnitPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  const token = localStorage.getItem("token");
+  const createProduct = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name: name,
+          unit_price: unit_price,
+          description: description,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const responseData = await response.json();
+      console.log(responseData);
+      setName("");
+      setUnitPrice("");
+      setDescription("");
+    } catch (error) {
+      console.log("error");
+      console.log(error.error);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={createProduct}>
+        <div className="mb-3">
+          <label for="name" className="form-label">
+            Product Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="unit_price" className="form-label">
+            Unit Price
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="unit_price"
+            value={unit_price}
+            onChange={(e) => {
+              setUnitPrice(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <label className="form-label" for="description">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CreateProductForm;
