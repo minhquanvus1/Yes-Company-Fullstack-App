c8f9eace3e5c01e623f3b4070eb59bb476aabbc3
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-21 15:58:51 +0700

    create the IntroPage as the page that Unauthenticated User will see at first

---
+++ frontend/YES-Company-Frontend/src/components/pages/IntroPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginButton from "../LoginButton.jsx";
+const IntroPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Yes Company</h1>
+      <p>Our Company sells the best products in the world</p>
+      <LoginButton></LoginButton>
+    </div>
+  );
+};
+
+export default IntroPage;
