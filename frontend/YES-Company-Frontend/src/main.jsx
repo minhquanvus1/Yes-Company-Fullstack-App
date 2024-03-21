bf2461c41ead43d3fe984720168e2fe4007e1195
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-21 09:57:55 +0700

    configure the Auth0Provider in main.jsx using the Auth0 Domain, and clientId

--- frontend/YES-Company-Frontend/src/main.jsx
+++ frontend/YES-Company-Frontend/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const domain = import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
+console.log(domain);
+console.log(clientId);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <BrowserRouter> */}
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "https://yesCompany/api",
+      }}
+    >
+      <App />
+    </Auth0Provider>
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
+);
