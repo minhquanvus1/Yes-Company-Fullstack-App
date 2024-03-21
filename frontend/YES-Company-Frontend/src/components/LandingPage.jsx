e9ae3e92f68d660139088792f63d943cab147dda
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-21 09:59:48 +0700

    create the LandingPage component for Authenticated user

---
+++ frontend/YES-Company-Frontend/src/components/LandingPage.jsx
@@ -0,0 +1,92 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React, { useEffect, useState } from "react";
+import CreateCustomerForm from "./CreateCustomerForm";
+import ProductList from "./ProductList";
+import CreateProductForm from "./CreateProductForm";
+import { useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+    useAuth0();
+  const [token, setAccessToken] = useState("");
+  const [isAlreadyCustomer, setIsAlreadyCustomer] = useState(false);
+  const navigate = useNavigate;
+
+  const getToken = async () => {
+    try {
+      const response = await getAccessTokenSilently();
+      setAccessToken(response);
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("isAuthenticated", isAuthenticated);
+      localStorage.setItem("token", response);
+      console.log(response);
+    } catch {
+      console.log("error");
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getToken();
+    }
+  }, [isAuthenticated]);
+
+  const checkCustomer = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/check-customer", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      if (response.status === 200) {
+        setIsAlreadyCustomer(true);
+      } else {
+        setIsAlreadyCustomer(false);
+      }
+      const responseData = await response.json();
+      console.log(responseData);
+    } catch (error) {
+      console.log("error");
+      console.log(error.error);
+    }
+  };
+
+  const handleCustomerFormClick = () => {
+    navigate;
+  };
+
+  return (
+    <div>
+      {isAuthenticated && (
+        <>
+          <h1>Welcome {user.name}</h1>
+          <button onClick={getToken}>Get Access Token</button>
+          <span>
+            <p>Access Token is {token}</p>
+          </span>
+          <button onClick={handleCustomerFormClick}>
+            Register to become our Customer
+          </button>
+          <button onClick={checkCustomer}>Are you already a Customer?</button>
+          <CreateProductForm></CreateProductForm>
+          {!isAlreadyCustomer && <ProductList></ProductList>}
+          {isAlreadyCustomer && (
+            <>
+              <p>You are already a customer</p>
+              <br />
+              <p>Here are the products</p>
+              <ProductList></ProductList>
+            </>
+          )}
+          {/* {!isAlreadyCustomer && <CreateCustomerForm token={token}/>} */}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LandingPage;
