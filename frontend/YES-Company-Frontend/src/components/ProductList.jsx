49e7060eac97523b55c9133e0cfedc8eb0c8018e
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-22 16:45:35 +0700

    add the logic about isLoading to only display the entire Product List when it has finished loading, and pass more props to CardProduct component

--- frontend/YES-Company-Frontend/src/components/ProductList.jsx
+++ frontend/YES-Company-Frontend/src/components/ProductList.jsx
@@ -2,9 +2,9 @@ import React from "react";
 // import { useAuth0 } from "@auth0/auth0-react";
 // import { useState, useEffect } from "react";
 import CardProduct from "./CardProduct";
-// import SpinnerLoading from "./utils/SpinnerLoading";
+import SpinnerLoading from "./utils/SpinnerLoading";
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, isLoading, role, setProducts }) => {
   //   const { isAuthenticated } = useAuth0();
   //   const [token, setToken] = useState(localStorage.getItem("token"));
   //   const [isLoading, setIsLoading] = useState(true);
@@ -41,34 +41,89 @@ const ProductList = ({ products }) => {
   //   };
 
   return (
+    // <div>
+    //   <h1>Product List</h1>
+    //   {isLoading && <SpinnerLoading></SpinnerLoading>}
+    //   {console.log(products)}
+    //   {console.log(products.length)}
+    //   {products.length > 0 ? (
+    //     <ul>
+    //       {products.map((product) => {
+    //         return (
+    //           <CardProduct
+    //             key={product.id}
+    //             id={product.id}
+    //             name={product.name}
+    //             unit_price={product.unit_price}
+    //             description={product.description}
+    //           />
+    //         );
+    //         // return <React.Fragment key={product.id}>
+    //         // <li>{product.name}</li>
+    //         // <li>{product.unit_price}</li>
+    //         // <li>{product.description}</li>
+    //         // </React.Fragment>
+    //       })}
+    //     </ul>
+    //   ) : (
+    //     <p>No products available</p>
+    //   )}
+    // </div>
     <div>
-      {/* {isLoading && <SpinnerLoading></SpinnerLoading>} */}
       <h1>Product List</h1>
-      {console.log(products)}
-      {console.log(products.length)}
-      {products.length > 0 ? (
-        <ul>
-          {products.map((product) => {
-            return (
-              <CardProduct
-                key={product.id}
-                id={product.id}
-                name={product.name}
-                unit_price={product.unit_price}
-                description={product.description}
-              />
-            );
-            // return <React.Fragment key={product.id}>
-            // <li>{product.name}</li>
-            // <li>{product.unit_price}</li>
-            // <li>{product.description}</li>
-            // </React.Fragment>
-          })}
-        </ul>
-      ) : (
-        <p>No products available</p>
+      {!isLoading && (
+        <div className="d-flex flex-wrap">
+          {products.length > 0 ? (
+            products.map((product) => (
+              <div key={product.id}>
+                <CardProduct
+                  id={product.id}
+                  name={product.name}
+                  unit_price={product.unit_price}
+                  description={product.description}
+                  role={role}
+                  setProducts={setProducts}
+                />
+              </div>
+            ))
+          ) : (
+            <p>No products available</p>
+          )}
+        </div>
       )}
+      {isLoading && <SpinnerLoading></SpinnerLoading>}
     </div>
+    // <div>
+    //   <h1>Product List</h1>
+    //   {!isLoading && (
+    //     <>
+    //       <div className="container">
+    //         <div className="row">
+    //           {products.length > 0 ? (
+    //             <div>
+    //               {products.map((product) => (
+    //                 <div className="col-md-4">
+    //                   <CardProduct
+    //                     key={product.id}
+    //                     id={product.id}
+    //                     name={product.name}
+    //                     unit_price={product.unit_price}
+    //                     description={product.description}
+    //                     role={role}
+    //                     setProducts={setProducts}
+    //                   />
+    //                 </div>
+    //               ))}
+    //             </div>
+    //           ) : (
+    //             <p>No products available</p>
+    //           )}
+    //         </div>
+    //       </div>
+    //     </>
+    //   )}
+    //   {isLoading && <SpinnerLoading></SpinnerLoading>}
+    // </div>
   );
 };
 
