8191d06183c54743c2f3ca737d033f4213c19431
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-22 16:46:40 +0700

    add the Link to CreateCustomerForm component for Authenticated user that is not already a Customer to register

--- frontend/YES-Company-Frontend/src/components/pages/views/CustomerView.jsx
+++ frontend/YES-Company-Frontend/src/components/pages/views/CustomerView.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import SpinnerLoading from "../../utils/SpinnerLoading";
 
-const CustomerView = ({ token }) => {
+const CustomerView = ({ token, isAlreadyCustomer, isLoading }) => {
   const { user, isAuthenticated } = useAuth0();
+  //   const [isAlreadyCustomer, setIsAlreadyCustomer] = useState(false);
+  //   const checkCustomer = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:8080/check-customer", {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       if (response.status === 200) {
+  //         setIsAlreadyCustomer(true);
+  //       } else {
+  //         setIsAlreadyCustomer(false);
+  //       }
+  //       const responseData = await response.json();
+  //       console.log(responseData);
+  //     } catch (error) {
+  //       console.log("error");
+  //       console.log(error.error);
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     checkCustomer();
+  //   }, []);
   return (
     <div>
       <div>
@@ -12,6 +40,10 @@ const CustomerView = ({ token }) => {
           {isAuthenticated ? `Welcome ${user.name}!` : "You are not logged in!"}
         </p>
       </div>
+      {!isLoading && isAuthenticated && !isAlreadyCustomer && (
+        <Link to="/customers/create">Register to be our Customer</Link>
+      )}
+      {isLoading && <SpinnerLoading />}
     </div>
   );
 };
