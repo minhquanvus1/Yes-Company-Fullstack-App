b4c6e5857c4669a56a69b4e77824c91b753b9fdb
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-23 22:20:54 +0700

    update the logic in checkCustomer() function to handle the case when the Authenticated user is not already a Customer in backend database, and re-throw this error to the Caller to handle it

--- frontend/YES-Company-Frontend/src/components/functions/checkCustomer.js
+++ frontend/YES-Company-Frontend/src/components/functions/checkCustomer.js
@@ -11,22 +11,27 @@ export const checkCustomer = async (
       },
     });
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      if (response.status === 404) {
+        throw new Error("This Customer not found in backend database");
+      } else {
+        throw new Error("Network response was not ok");
+      }
     }
     const responseData = await response.json();
-    console.log(responseData);
+    console.log("response data of checkCustomer():", responseData);
     if (response.status === 200) {
       const customer = responseData.customer;
       console.log("customer: ", customer);
       setIsAlreadyCustomer(true);
+      setIsLoading(false);
       return customer;
-    } else {
-      setIsAlreadyCustomer(false);
-      console.log("message: ", responseData.message);
     }
-    setIsLoading(false);
+    // setIsLoading(false);
   } catch (error) {
+    setIsAlreadyCustomer(false);
+    setIsLoading(false);
     console.log("error");
-    console.log(error.error);
+    console.log(error.message);
+    throw error;
   }
 };
