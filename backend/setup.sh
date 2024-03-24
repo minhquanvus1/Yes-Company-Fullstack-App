6e9a9bd2a25b54df25b8d9558e2957d328998822
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-03-24 22:54:35 +0700

    export the Auth0 credentials in setup.sh file

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+export AUTH0_DOMAIN='dev-tioi4bnfisc6bcli.us.auth0.com'
+export ALGORITHMS="['RS256']"
+export API_AUDIENCE='https://yesCompany/api'
+
+echo 'hello world from setup.sh'
+echo $AUTH0_DOMAIN
+# echo ${ALGORITHMS[@]}
+echo $API_AUDIENCE
+echo $ALGORITHMS
